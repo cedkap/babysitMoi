56817aab0272297816000c74940844d71e3e9753
cedkap <cedrick.kapemashako2018@campus-eni.fr>
2019-03-22 21:33:50 +0100

    4 files modified

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
  { path: '', redirectTo: 'home', pathMatch: 'full' },
- { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+ { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
+ { path: 'contact', loadChildren: './pages/contact/contact.module#ContactPageModule' },
 ];
 
 @NgModule({
  imports: [RouterModule.forRoot(routes)],
  exports: [RouterModule]
 })
+export class AppRoutingModule { }
